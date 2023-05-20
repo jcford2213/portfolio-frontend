7e61112a30d9e502bb38304dfdc288f3e04848d4
IdioticMarlin22 <jcrantfo@comcast.net>
2023-05-19 21:17:42 -0400

    Changed APOD Project Image Hover Effects to Project Figure Enabled

--- src/components/projectsComponent.vue
+++ src/components/projectsComponent.vue
@@ -13,21 +13,17 @@ export default {
   <div id="projects-wrapper">
     <h3 class="headline">An Example of My Work</h3>
     <section v-for="project in projects">
-      <figure class="project-banner">
-        <img id="project-image" :src="project.snapshot" alt="Project Screenshot">
-        <div class="overlay"></div>        
-        <figcaption class="project-name headline">
-          {{ project.name }}
+      <figure class="project-figure">
+        <img class="project-image" :src="project.snapshot" alt="Project Screenshot">      
+        <figcaption class="project-figcaption headline">
+          <p class="paragraph description">
+            {{ project.description }}
+          </p>
+          <button class="nav-button link-to-project">
+            <a v-bind:href="project.link" target="_blank">Visit Site ></a>
+          </button>
         </figcaption>
       </figure>
-      <div v-if="false">
-        <p class="paragraph" id="description">
-          {{ project.description }}
-        </p>
-        <button class="nav-button">
-          <a v-bind:href="project.link" target="_blank">Visit Site ></a>
-        </button>
-      </div>
     </section>
   </div>
 </template>
@@ -41,43 +37,61 @@ export default {
   margin-block: 3.125rem;
   margin-inline: 10%;
 }
-.project-banner {
+.project-figure {
   display: flex;
   justify-content: center;
   align-items: center;
   row-gap: 1rem;
   position: relative;
   background-color: #000000;
-  margin-block: 1rem;
   margin-inline: auto;
   max-width: 800px;
-}
-.project-banner > figcaption {
-  position: absolute;
-}
-.project-name {
-  text-align: center;
-  padding: 3.5rem 1.5rem;
-  font-size: 1.6rem;
+  overflow: hidden;
 }
 .project-image{
   width: 100%;
   height: auto;
   aspect-ratio: attr(width) / attr(height);
+  transition: .25s ease-in-out;
 }
-.overlay {
+.project-figcaption {
   position: absolute;
-  top: 0;
-  bottom: 0;
   width: 100%;
   height: 100%;
-  background-color: #000000;
-  opacity: 0.7;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-size: 1.2rem;
+  row-gap: 0.6rem;
+  padding: 3.5rem 1.5rem;
+  background-color: #404040;
+  opacity: 0;
+  transition: .45s ease-in-out;
 }
-#description {
-  padding-inline: 2rem;
+.project-figure:hover .project-figcaption {
+  opacity: 1;
+}
+.project-figure:hover .project-image {
+  transform: scale(1.1);
+}
+.description-div {
+  
+}
+.link-to-project {
+  max-width: 70%;
 }
 
+
 @media screen and (min-width: 800px) {
+  .project-figcaption {
+    padding-inline: 10%;
+    row-gap: 2.5rem;
+    font-size: 1.6rem;
+  }
+  .description {
+    padding-inline: 2rem;
+  }
 }
 </style>
