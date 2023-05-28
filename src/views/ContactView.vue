d91e29c35078f2e4ade7a0dbc1469358643cbaf1
IdioticMarlin22 <jcrantfo@comcast.net>
2023-05-28 11:54:17 -0400

    Email form on new branch with current styles

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -33,9 +33,22 @@ export default {
 
 <template>
   <div id="contact-wrapper">
-    <h2 class="headline">Get in touch!</h2>
-    <h2 class="headline">Email me at:</h2>
-    <a href="mailto: jackson-crantford@outlook.com" class="paragraph">jackson-crantford@outlook.com</a>
+    <h1 class="headline"></h1>
+    <form id="email-form" @submit.prevent="sendMessage(formValues)">
+      <div>
+        <label class="paragraph" for="name">Name</label>
+        <input type="text" name="name" id="name-input" v-model="formValues.name">
+      </div>
+      <div>
+        <label class="paragraph" for="email">Email</label>
+        <input type="email" name="email" id="email-input" v-model="formValues.email">
+      </div>
+      <div>
+        <label class="paragraph" for="name">Message</label>
+        <textarea name="message" id="message-Input" rows="10" v-model="formValues.message"></textarea>
+      </div>
+      <button class="nav-button" type="submit">Send</button>
+    </form>
   </div>
 </template>
 
@@ -44,16 +57,7 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: start;
-  row-gap: 1rem;
-  margin-top: 4rem;
-  margin-inline: auto;
-}
-a {
-  font-size: 1.25rem;;
-}
-a:hover {
-  color: #1DB954;
+  align-items: center;
 }
 #email-form {
   display: flex;
