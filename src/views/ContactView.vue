9df5c8c621d422912044da7fc9d09e4dd8e31877
IdioticMarlin22 <jcrantfo@comcast.net>
2023-05-29 12:15:45 -0400

    Created Javascript form validation and styles

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -10,22 +10,66 @@ export default {
     }
   },
   methods: {
-    async sendMessage(message = {}) {
-      const server = 'http://localhost:3000';
-      const response = await fetch(server, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(message)
+    setInputError (keyName, message) {
+      const element = document.getElementById(`${keyName}-input`);
+      const inputControl = element.parentElement;
+      const errorDisplay = inputControl.querySelector('.error');
+      errorDisplay.innerText = message;
+      inputControl.classList.add('error');
+      inputControl.classList.remove('success');
+    },
+    setInputSuccess (keyName) {
+      const element = document.getElementById(`${keyName}-input`);
+      const inputControl = element.parentElement;
+      const errorDisplay = inputControl.querySelector('.error');
+      errorDisplay.innerText = '';
+      inputControl.classList.add('success');
+      inputControl.classList.remove('error');
+    },
+    validateInputs (formInput = {}) {
+      return new Promise((resolve, reject) => {
+        let shouldValidateReject = false;
+        for (const [key, value] of Object.entries(formInput)) {
+          if (value === '') {
+            const message = `${key} is required.`
+            this.setInputError(key, message);
+            shouldValidateReject = true;
+            continue;
+          }
+          if (key === 'email' && !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+            const message = `Please enter a valid email address.`
+            this.setInputError(key, message);
+            shouldValidateReject = true;
+            continue;
+          }
+          this.setInputSuccess(key);
+        }
+        if (shouldValidateReject === true) {
+          reject();
+        }
+        resolve()
       })
-      .catch(err => {
-        return err.message;
-      });
-      this.$router.push('/');
-      return response.data;
+    },
+    async sendToServer (formInput = {}) {
+      try {
+        await this.validateInputs(formInput);
+        // const server = 'http://localhost:3000';
+        // const response = await fetch(server, {
+        //   method: 'POST',
+        //   mode: 'cors',
+        //   cache: 'no-cache',
+        //   headers: {
+        //     'Content-Type': 'application/json'
+        //   },
+        //   body: JSON.stringify(formInput)
+        // })
+        // this.$router.push('/');
+        // return response.data;
+        
+      }
+      catch(err) {
+        
+      };
     }
   }
 }
@@ -33,19 +77,23 @@ export default {
 
 <template>
   <div id="contact-wrapper">
-    <h1 class="headline"></h1>
-    <form id="email-form" @submit.prevent="sendMessage(formValues)">
-      <span>
+    
+    <form id="email-form" @submit.prevent="sendToServer(formValues)">
+      <h1 class="headline"></h1>
+      <span class="input-control">
         <label class="paragraph" for="name">Name</label>
         <input type="text" name="name" id="name-input" v-model="formValues.name">
+        <div class="error paragraph"></div>
       </span>
-      <span>
+      <span class="input-control">
         <label class="paragraph" for="email">Email</label>
-        <input type="email" name="email" id="email-input" v-model="formValues.email">
+        <input type="text" name="email" id="email-input" v-model="formValues.email">
+        <div class="error paragraph"></div>
       </span>
-      <span>
+      <span class="input-control">
         <label class="paragraph" for="name">Message</label>
-        <textarea name="message" id="message-Input" rows="10" v-model="formValues.message"></textarea>
+        <textarea name="message" id="message-input" rows="10" v-model="formValues.message"></textarea>
+        <div class="error paragraph"></div>
       </span>
       <button class="nav-button" type="submit">Send</button>
     </form>
@@ -68,23 +116,31 @@ export default {
   padding-inline: 0.5rem;
   margin-block: 1rem;
 }
-#email-form span {
+.input-control {
   display: flex;
   flex-direction: column;
 }
-input, textarea {
+.input-control input, .input-control textarea {
   padding: 0.5rem;
   border: 0.2rem solid #028090;
   border-radius: 5px;
   margin-top: 0.675rem;
 }
-textarea {
+.input-control textarea {
   resize: vertical;
 }
-input:focus, textarea:focus {
+.input-control input:focus, .input-control textarea:focus {
   outline: none;
-  border: none;
-  box-shadow: 0px 0px 3px 3px #1DB954;
+}
+.input-control.success input, .input-control.success textarea {
+  border-color: #1DB954;
+}
+.input-control.error input, .input-control.error textarea {
+  border-color: red;
+}
+.input-control .error {
+  color: red;
+  font-size: 1rem;
 }
 #email-form button {
   width: fit-content;
